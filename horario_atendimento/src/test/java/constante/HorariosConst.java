9cef7c908ebd81ae992eec6ae5ea2b1fc0ebf955
PedroBorem <pedro.borem@outlook.com>
2024-03-18 15:09:31 -0300

    test: Horarios Constantes Mock

---
+++ horario_atendimento/src/test/java/constante/HorariosConst.java
@@ -0,0 +1,33 @@
+package constante;
+
+public class HorariosConst {
+    /* Predio ->  
+    1-5 = \"1\",
+    6-10 = \"2\",
+    11-15 = \"3\",
+    16-20 = \"4\",
+    21-25 = \"6\"
+    */
+
+    public static String CHRIS =
+        "{ \"nomeDoProfessor\":\"Chris Lima\", \n " +
+                "\"horarioDeAtendimento\":\"17:30\", \n " +
+                "\"periodo\":\"integral\", \n " +
+                "\"sala\":\"3\",\n " +
+                "\"predio\":\"1\"}";
+
+    public static String RENZO =
+    "{ \"nomeDoProfessor\":\"Renzo\", \n " +
+            "\"horarioDeAtendimento\":\"19:30\", \n " +
+            "\"periodo\":\"noturno\", \n " +
+            "\"sala\":\"20\",\n " +
+            "\"predio\":\"4\"}";
+
+    public static String INEXISTENTE =
+    "{ \"nomeDoProfessor\":\"Inexistente\", \n " +
+            "\"horarioDeAtendimento\":\"Inexistente\", \n " +
+            "\"periodo\":\"Inexistente\", \n " +
+            "\"sala\":\"Inexistente\",\n " +
+            "\"predio\":\"Inexistente\"}";
+
+}
