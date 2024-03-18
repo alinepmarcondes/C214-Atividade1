e7decf30e79609e332952fea512edd4d65b1ae6d
alinepmarcondes <aline.patta@ges.inatel.br>
2024-03-18 15:10:46 -0300

    :recycle: test: testeBuscaHorarioChris, TesteBuscaHorarioInvalido, testeBuscaHorarioValido

--- horario_atendimento/src/test/java/mockito/TesteBuscaHorario.java
+++ horario_atendimento/src/test/java/mockito/TesteBuscaHorario.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,7 +28,38 @@ public class TesteBuscaHorario {
     }
     
     @Test
+    public void testeBuscaHorarioA(){
+        Mockito.when(service.busca(55)).thenReturn(HorarioConst.CHRIS);
+
+        Horarios chris = buscaHorario.buscaHorarios(55);
+        //Faz assertion
+        assertEquals("Chris Lima", chris.getNome());
+        assertEquals("17:30", chris.getHorarioDeAtendimento());
+        assertEquals("E", chris.getPeriodo());
+        assertEquals("a", chris.getSala());
+        assertEquals("a", chris.getPredio());
+    }
+
+    @Test
     public void TesteBuscaHorarioInvalido(){
-        
+        Mockito.when(service.busca(56)).thenReturn(HorarioConst.INEXISTENTE);
+
+        Horarios inexistente = buscaHorario.buscaHorarios(56);
+        //Faz assertion
+        assertEquals("chris", inexistente.getNome());
+        assertEquals("17:30", inexistente.getHorarioDeAtendimento());
+        assertEquals("E", inexistente.getPeriodo());
+        assertEquals("a", inexistente.getSala());
+        assertEquals("a", inexistente.getPredio());
+    }
+
+    @Test
+    public void testeBuscaInimigoValido(){
+
+        Mockito.when(service.horarioExistente(10)).thenReturn(true);
+        //Faz a busca de um horário válido
+        boolean horarioValido = service.horarioExistente(10);
+
+        assertTrue(horarioValido);
     }
 }
