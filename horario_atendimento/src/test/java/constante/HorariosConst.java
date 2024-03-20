5891e3e8241ba62ef13ecd667c8dea582478d49a
alinepmarcondes <aline.patta@ges.inatel.br>
2024-03-20 13:51:25 -0300

    :recycle: test: update mock horarios const

--- horario_atendimento/src/test/java/constante/HorariosConst.java
+++ horario_atendimento/src/test/java/constante/HorariosConst.java
@@ -9,25 +9,46 @@ public class HorariosConst {
     21-25 = \"6\"
     */
 
-    public static String CHRIS =
+        public static String CHRIS =
         "{ \"nomeDoProfessor\":\"Chris Lima\", \n " +
                 "\"horarioDeAtendimento\":\"17:30\", \n " +
                 "\"periodo\":\"integral\", \n " +
                 "\"sala\":\"3\",\n " +
                 "\"predio\":\"1\"}";
 
-    public static String RENZO =
-    "{ \"nomeDoProfessor\":\"Renzo\", \n " +
-            "\"horarioDeAtendimento\":\"19:30\", \n " +
-            "\"periodo\":\"noturno\", \n " +
-            "\"sala\":\"20\",\n " +
-            "\"predio\":\"4\"}";
+        public static String RENZO =
+        "{ \"nomeDoProfessor\":\"Renzo\", \n " +
+                "\"horarioDeAtendimento\":\"19:30\", \n " +
+                "\"periodo\":\"noturno\", \n " +
+                "\"sala\":\"20\",\n " +
+                "\"predio\":\"4\"}";
 
-    public static String INEXISTENTE =
-    "{ \"nomeDoProfessor\":\"Inexistente\", \n " +
-            "\"horarioDeAtendimento\":\"Inexistente\", \n " +
-            "\"periodo\":\"Inexistente\", \n " +
-            "\"sala\":\"Inexistente\",\n " +
-            "\"predio\":\"Inexistente\"}";
+        public static String MARCELO =
+        "{ \"nomeDoProfessor\":\"Marcelo\", \n " +
+                "\"horarioDeAtendimento\":\"15:30\", \n " +
+                "\"periodo\":\"integral\", \n " +
+                "\"sala\":\"7\",\n " +
+                "\"predio\":\"2\"}";
+        
+        public static String YVO =
+        "{ \"nomeDoProfessor\":\"Yvo\", \n " +
+                "\"horarioDeAtendimento\":\"21:30\", \n " +
+                "\"periodo\":\"noturno\", \n " +
+                "\"sala\":\"15\",\n " +
+                "\"predio\":\"3\"}";
+
+        public static String SONED =
+        "{ \"nomeDoProfessor\":\"Soned\", \n " +
+                "\"horarioDeAtendimento\":\"9:30\", \n " +
+                "\"periodo\":\"integral\", \n " +
+                "\"sala\":\"23\",\n " +
+                "\"predio\":\"6\"}";     
+
+        public static String INEXISTENTE =
+        "{ \"nomeDoProfessor\":\"Inexistente\", \n " +
+                "\"horarioDeAtendimento\":\"Inexistente\", \n " +
+                "\"periodo\":\"Inexistente\", \n " +
+                "\"sala\":\"Inexistente\",\n " +
+                "\"predio\":\"Inexistente\"}";
 
 }
